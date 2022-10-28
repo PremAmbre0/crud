ccae5ee75958fd4efdce0a9601da48f504c576ab
zoro-2002 <premambre80@gmail.com>
2022-10-28 13:36:52 +0530

    add : added snackbar

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,51 @@
 
     <v-main>
       <router-view />
+      <v-snackbar v-model="localSnackbarState" multi-line light :timeout="snackbarTime" text :color="snackbarColor" top>
+        {{ snackbarText }}
+        <template v-slot:action="{ attrs }">
+          <v-btn text v-bind="attrs" @click.stop="closeSnackbar" icon>
+            <v-icon color="#091E42">mdi-close</v-icon>
+          </v-btn>
+        </template>
+
+      </v-snackbar>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import { mapMutations, mapGetters } from 'vuex';
+
 export default {
   name: "App",
 
   data: () => ({
-    //
+    localSnackbarState: false,
   }),
+  methods: {
+    ...mapMutations(["openSnackbar", "closeSnackbar"]),
+  },
+  computed: {
+    ...mapGetters(["snackbarState", "snackbarText", "snackbarTime", "snackbarColor"]),
+  },
+  watch: {
+    snackbarState(nv) {
+      this.localSnackbarState = nv;
+    },
+    localSnackbarState(nv) {
+      if (!nv) {
+        this.closeSnackbar();
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.nav-header-text{
-  color:$white;
+.nav-header-text {
+  color: $white;
   font-weight: 600;
   font-size: 1.7rem;
 }
-
 </style>
