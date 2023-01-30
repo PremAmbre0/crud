cb6de81ed707c4656d7bb353e0541175cd1f4ede
Taher Pardawala <skypunch2@gmail.com>
2023-01-30 17:19:43 +0530

    add: form integration for dynamic product properties

--- src/components/DialogForm.vue
+++ src/components/DialogForm.vue
@@ -1,237 +1,357 @@
 <template>
-    <div>
-        <v-dialog v-model="showDialogForm" max-width="600" persistent>
-            <v-card max-width="600">
-                <v-form @submit.prevent>
-                    <v-text-field width="50vw" :rules="rules.notEmpty" required outlined placeholder="name" label="name"
-                        v-model="formData.name">
-                    </v-text-field>
-                    <v-text-field width="50vw" :rules="rules.notEmpty" required outlined placeholder="projectId"
-                        label="projectId" v-model="formData.projectId">
-                    </v-text-field>
-                    <v-text-field width="50vw" :rules="rules.notEmpty" required outlined placeholder="roomType"
-                        label="roomType" v-model="formData.roomType">
-                    </v-text-field>
-                    <v-textarea outlined name="input-7-4" label="Description" v-model="formData.description">
-                    </v-textarea>
-                    <v-combobox
-                        v-model="formData.allowedEmails"
-                        :items="formData.allowedEmails"
-                        :search-input.sync="search"
-                        hide-selected
-                        label="Add Allowed Emails"
-                        multiple
-                        persistent-hint
-                        small-chips
-                        clearable
-                    >
-                        <template v-slot:no-data>
-                            <v-list-item>
-                            <v-list-item-content>
-                                <v-list-item-title>
-                                No results matching "<strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a new one
-                                </v-list-item-title>
-                            </v-list-item-content>
-                            </v-list-item>
-                        </template>
-                    </v-combobox>
-                    <div class="droparea" @dragover.prevent @drop.stop.prevent>
-                        <div class="droparea-filled" v-if="existingPreviewImage || inputedPreviewImage">
-                            <div class="file-wrapper">
-                                <v-btn class="close-btn" tile elevation="2" x-small @click="removeImage()">
-                                    <v-icon>mdi-close</v-icon>
-                                </v-btn>
-                                <img ref="fileInput" class="img"
-                                    :src="existingPreviewImage ? existingPreviewImage : inputedPreviewImage" />
-                            </div>
-                        </div>
-                        <div class="droparea-empty" v-else>
-                            <label for="fileInput" class="label">
-                                Click here to select and preview the file</label>
-                            <input ref="fileInput" type="file" @input="pickImagefile($event)" id="fileInput" />
-                        </div>
-                    </div>
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn plain color="#BF2600" @click="closeDialogForm()">cancel</v-btn>
-                        <v-btn plain color="#00875A" type="submit" @click="submitFormData()">submit</v-btn>
-                    </v-card-actions>
-                </v-form>
-            </v-card>
-        </v-dialog>
-        <v-overlay :value="showOverlayLoader">
-            <v-progress-circular indeterminate color="#5243AA" size="64"></v-progress-circular>
-        </v-overlay>
-    </div>
+	<div>
+		<v-dialog v-model="showDialogForm" max-width="600" persistent>
+			<v-card max-width="600">
+				<v-form @submit.prevent>
+					<v-text-field
+						width="50vw"
+						:rules="rules.notEmpty"
+						required
+						outlined
+						placeholder="name"
+						label="name"
+						v-model="formData.name"
+					>
+					</v-text-field>
+					<v-text-field
+						width="50vw"
+						:rules="rules.notEmpty"
+						required
+						outlined
+						placeholder="projectId"
+						label="projectId"
+						v-model="formData.projectId"
+					>
+					</v-text-field>
+					<v-text-field
+						width="50vw"
+						:rules="rules.notEmpty"
+						required
+						outlined
+						placeholder="roomType"
+						label="roomType"
+						v-model="formData.roomType"
+					>
+					</v-text-field>
+					<v-textarea
+						outlined
+						name="input-7-4"
+						label="Description"
+						v-model="formData.description"
+					>
+					</v-textarea>
+					<template v-for="filter of fabricPresetFilters.properties">
+						<v-autocomplete
+							:key="filter.formName"
+							v-model="formData[filter.formName]"
+							:items="filter.children"
+							hide-selected
+							:label="filter.name"
+							item-text="name"
+							item-value="formName"
+							persistent-hint
+							small-chips
+							clearable
+							outlined
+						></v-autocomplete>
+					</template>
+					<v-combobox
+						v-model="formData.allowedEmails"
+						:items="formData.allowedEmails"
+						:search-input.sync="search"
+						hide-selected
+						label="Add Allowed Emails"
+						multiple
+						persistent-hint
+						small-chips
+						clearable
+					>
+						<template v-slot:no-data>
+							<v-list-item>
+								<v-list-item-content>
+									<v-list-item-title>
+										No results matching "<strong>{{
+											search
+										}}</strong
+										>". Press <kbd>enter</kbd> to create a
+										new one
+									</v-list-item-title>
+								</v-list-item-content>
+							</v-list-item>
+						</template>
+					</v-combobox>
+					<div class="droparea" @dragover.prevent @drop.stop.prevent>
+						<div
+							class="droparea-filled"
+							v-if="existingPreviewImage || inputedPreviewImage"
+						>
+							<div class="file-wrapper">
+								<v-btn
+									class="close-btn"
+									tile
+									elevation="2"
+									x-small
+									@click="removeImage()"
+								>
+									<v-icon>mdi-close</v-icon>
+								</v-btn>
+								<img
+									ref="fileInput"
+									class="img"
+									:src="
+										existingPreviewImage
+											? existingPreviewImage
+											: inputedPreviewImage
+									"
+								/>
+							</div>
+						</div>
+						<div class="droparea-empty" v-else>
+							<label for="fileInput" class="label">
+								Click here to select and preview the file</label
+							>
+							<input
+								ref="fileInput"
+								type="file"
+								@input="pickImagefile($event)"
+								id="fileInput"
+							/>
+						</div>
+					</div>
+					<v-card-actions>
+						<v-spacer></v-spacer>
+						<v-btn plain color="#BF2600" @click="closeDialogForm()"
+							>cancel</v-btn
+						>
+						<v-btn
+							plain
+							color="#00875A"
+							type="submit"
+							@click="submitFormData()"
+							>submit</v-btn
+						>
+					</v-card-actions>
+				</v-form>
+			</v-card>
+		</v-dialog>
+		<v-overlay :value="showOverlayLoader">
+			<v-progress-circular
+				indeterminate
+				color="#5243AA"
+				size="64"
+			></v-progress-circular>
+		</v-overlay>
+	</div>
 </template>
 
 <script>
-
-import { mapGetters, mapMutations, mapActions } from 'vuex';
+import { mapGetters, mapMutations, mapActions } from "vuex";
 
 export default {
-    props: {
-        roomStyle: {
-            type: Object,
-            required: false,
-        }
-    },
-    data() {
-        return {
-            formData: {},
-            inputedPreviewImage: null,
-            existingPreviewImage: null,
-            inputedFileObject: null,
-            mode: '',
-            rules: {
-                notEmpty: [val => (val || '').length > 0 || 'This field is required'],
-            },
-            search: null,
-        }
-    },
-    computed: {
-        ...mapGetters(["showDialogForm", "showOverlayLoader"]),
-    },
-    watch: {
-        showDialogForm(newValue) {
-            if (newValue == true) {
-                this.setInitialFormData();
-            }
-            if (newValue == false) {
-                this.inputedFileObject = null;
-                this.inputedPreviewImage = null,
-                    this.existingPreviewImage = null,
-                    this.resetFormData();
-            }
-        }
-    },
-    methods: {
-        ...mapMutations(["closeDialogForm", "openOverlayLoader"]),
-        ...mapActions("crud", ["postRoomStyles", "updateRoomStyles"]),
-        setInitialFormData() {
-            if (this.roomStyle) {
-                this.formData = JSON.parse(JSON.stringify(this.roomStyle));
-                this.existingPreviewImage = this.roomStyle.thumbnail;
-                this.mode = "edit";
-            } else {
-                this.existingPreviewImage = "";
-                this.mode = "new";
-            }
-        },
-        pickImagefile(e) {
-            let inputedImage = e.target.files[0]
-            if (inputedImage) {
-                this.inputedFileObject = inputedImage;
-                this.inputedPreviewImage = URL.createObjectURL(inputedImage);
-            }
-        },
-        resetFormData() {
-            this.formData = {}
-        },
-        removeImage() {
-            if (this.inputedPreviewImage) {
-                this.inputedPreviewImage = ""
-            } else if (this.existingPreviewImage) {
-                this.existingPreviewImage = ""
-            }
-        },
-        submitFormData() {
-            this.openOverlayLoader();
-            let data = new FormData();
-            this.formData.description ? data.append('description', this.formData.description.trim()) :  data.append('description', "") 
-            this.formData.projectId ? data.append('projectId', this.formData.projectId.trim()) : data.append('projectId', this.formData.projectId);
-            this.formData.name ? data.append('name', this.formData.name.trim()) : data.append('name', "");
-            this.formData.roomType ? data.append('roomType', this.formData.roomType.trim()) : data.append('roomType', this.formData.roomType);
-            this.formData.allowedEmails ? data.append('allowedEmails', JSON.stringify(this.formData.allowedEmails)) : data.append('allowedEmails', JSON.stringify([]));
-            data.append('file', this.inputedFileObject);
-            if (this.mode == "new") {
-                this.postRoomStyles(data)
-                    .then((response) => {
-                        console.log(response)
-                        this.$emit('refresh');
-                    })
-            } else {
-                data.append('_id', this.formData._id);
-                console.log(...data)
-                this.updateRoomStyles(data)
-                    .then((response) => {
-                        console.log(response)
-                        this.$emit('refresh');
-                    })
-            }
-            this.closeDialogForm();
-        }
-    }
-}
+	props: {
+		roomStyle: {
+			type: Object,
+			required: false,
+		},
+	},
+	data() {
+		return {
+			formData: {},
+			inputedPreviewImage: null,
+			existingPreviewImage: null,
+			inputedFileObject: null,
+			mode: "",
+			rules: {
+				notEmpty: [
+					(val) => (val || "").length > 0 || "This field is required",
+				],
+			},
+			search: null,
+		};
+	},
+	computed: {
+		...mapGetters(["showDialogForm", "showOverlayLoader"]),
+		...mapGetters("crud", ["dynamicProperties"]),
+		fabricPresetFilters() {
+			const fabricPresetFilters = this.dynamicProperties.find(
+				(e) => e.formName == "fABRICPRESETFILTERS"
+			);
+
+			if (!fabricPresetFilters) return {};
+			return fabricPresetFilters;
+		},
+	},
+	watch: {
+		showDialogForm(newValue) {
+			if (newValue == true) {
+				this.setInitialFormData();
+			}
+			if (newValue == false) {
+				this.inputedFileObject = null;
+				(this.inputedPreviewImage = null),
+					(this.existingPreviewImage = null),
+					this.resetFormData();
+			}
+		},
+	},
+	methods: {
+		...mapMutations(["closeDialogForm", "openOverlayLoader"]),
+		...mapActions("crud", ["postRoomStyles", "updateRoomStyles"]),
+		setInitialFormData() {
+			if (this.roomStyle) {
+				this.formData = JSON.parse(JSON.stringify(this.roomStyle));
+				this.existingPreviewImage = this.roomStyle.thumbnail;
+				this.mode = "edit";
+			} else {
+				this.existingPreviewImage = "";
+				this.mode = "new";
+			}
+
+			for (const filter of this.fabricPresetFilters.properties) {
+				if (!this.formData[filter.formName]) {
+					this.$set(
+						this.formData,
+						filter.formName,
+						filter.children.length > 0
+							? filter.children[0].formName
+							: ""
+					);
+				}
+			}
+		},
+		pickImagefile(e) {
+			let inputedImage = e.target.files[0];
+			if (inputedImage) {
+				this.inputedFileObject = inputedImage;
+				this.inputedPreviewImage = URL.createObjectURL(inputedImage);
+			}
+		},
+		resetFormData() {
+			this.formData = {};
+		},
+		removeImage() {
+			if (this.inputedPreviewImage) {
+				this.inputedPreviewImage = "";
+			} else if (this.existingPreviewImage) {
+				this.existingPreviewImage = "";
+			}
+		},
+		submitFormData() {
+			this.openOverlayLoader();
+			let data = new FormData();
+			this.formData.description
+				? data.append("description", this.formData.description.trim())
+				: data.append("description", "");
+			this.formData.projectId
+				? data.append("projectId", this.formData.projectId.trim())
+				: data.append("projectId", this.formData.projectId);
+			this.formData.name
+				? data.append("name", this.formData.name.trim())
+				: data.append("name", "");
+			this.formData.roomType
+				? data.append("roomType", this.formData.roomType.trim())
+				: data.append("roomType", this.formData.roomType);
+			this.formData.allowedEmails
+				? data.append(
+						"allowedEmails",
+						JSON.stringify(this.formData.allowedEmails)
+				  )
+				: data.append("allowedEmails", JSON.stringify([]));
+
+			for (const filter of this.fabricPresetFilters.properties) {
+				if (this.formData[filter.formName]) {
+					data.append(
+						filter.formName,
+						JSON.stringify(this.formData[filter.formName])
+					);
+				}
+			}
+			data.append("file", this.inputedFileObject);
+			if (this.mode == "new") {
+				this.postRoomStyles(data).then((response) => {
+					console.log(response);
+					this.$emit("refresh");
+				});
+			} else {
+				data.append("_id", this.formData._id);
+				console.log(...data);
+				this.updateRoomStyles(data).then((response) => {
+					console.log(response);
+					this.$emit("refresh");
+				});
+			}
+			this.closeDialogForm();
+		},
+	},
+};
 </script>
 
 <style lang="scss" scoped>
 .v-card {
-    padding: 5%;
+	padding: 5%;
 }
 
 .droparea {
-    display: block;
-    height: 50vh;
-    width: 100%;
+	display: block;
+	height: 50vh;
+	width: 100%;
 
-    &-empty {
-        height: 100%;
-        width: 100%;
-        background-color: $light-three;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 1.2rem;
-        padding: 5%;
-        text-align: center;
-        color: $light-one;
-        font-weight: 500;
-        border-radius: 0.5rem;
-        position: relative;
-    }
+	&-empty {
+		height: 100%;
+		width: 100%;
+		background-color: $light-three;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 1.2rem;
+		padding: 5%;
+		text-align: center;
+		color: $light-one;
+		font-weight: 500;
+		border-radius: 0.5rem;
+		position: relative;
+	}
 
-    &-filled {
-        display: flex;
-        justify-content: center;
-        height: 90%;
-    }
+	&-filled {
+		display: flex;
+		justify-content: center;
+		height: 90%;
+	}
 }
 
 .img {
-    object-fit: contain;
-    height: 100%;
-    max-width: 100%;
+	object-fit: contain;
+	height: 100%;
+	max-width: 100%;
 }
 
 .label {
-    display: flex;
-    height: 100%;
-    width: 100%;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
+	display: flex;
+	height: 100%;
+	width: 100%;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
 }
 
 input[type="file"] {
-    height: 100%;
-    width: 100%;
-    opacity: 0;
-    position: absolute;
+	height: 100%;
+	width: 100%;
+	opacity: 0;
+	position: absolute;
 }
 
 .file-wrapper {
-    position: relative;
+	position: relative;
 }
 
 .v-btn.close-btn {
-    position: absolute;
-    top: 0;
-    right: 0;
+	position: absolute;
+	top: 0;
+	right: 0;
 }
 
 .v-icon {
-    font-size: 1rem;
+	font-size: 1rem;
 }
 </style>
-    
--- src/views/Crud.vue
+++ src/views/Crud.vue
@@ -1,166 +1,216 @@
 <template>
-    <div>
-        <div class="grouping-container" v-for="roomType in roomsGroupedByRoomStyles" :key="roomType">
-            <div class="grouping-container-category">{{ roomType + `(${roomsGroupedByRoomStyles[roomType].length})` }}
-            </div>
-            <div class="cards-container">
-                <card v-for="roomStyle in  roomsGroupedByRoomStyles[roomType]" :key="roomStyle._id"
-                    :name="roomStyle.name" :thumbnail="roomStyle.thumbnail" :roomStyleDisabled="roomStyle.isDisabled"
-                    :id="roomStyle._id" @openForm="openDialogForm() ; setSelectedRoomStyle(roomStyle)"
-                    @delete="deleteData(roomStyle._id)" @moveRenders="handleMoveRenders(roomStyle._id)"
-                    @publishOptions="handlePublishOptions(roomStyle._id)">
-                </card>
-            </div>
-        </div>
-        <v-btn class="add-thumbnail" fab dark color="indigo" @click="removeselectedTemplate() ; openDialogForm()">
-            <v-icon dark> mdi-plus </v-icon>
-        </v-btn>
-        <dialog-form :roomStyle="selectedRoomStyle" @refresh="getData();"></dialog-form>
-        <v-overlay :value="showOverlayLoader">
-            <v-progress-circular indeterminate color="#5243AA" size="64"></v-progress-circular>
-        </v-overlay>
-    </div>
+	<div>
+		<div
+			class="grouping-container"
+			v-for="roomType in roomsGroupedByRoomStyles"
+			:key="roomType"
+		>
+			<div class="grouping-container-category">
+				{{
+					roomType + `(${roomsGroupedByRoomStyles[roomType].length})`
+				}}
+			</div>
+			<div class="cards-container">
+				<card
+					v-for="roomStyle in roomsGroupedByRoomStyles[roomType]"
+					:key="roomStyle._id"
+					:name="roomStyle.name"
+					:thumbnail="roomStyle.thumbnail"
+					:roomStyleDisabled="roomStyle.isDisabled"
+					:id="roomStyle._id"
+					@openForm="
+						openDialogForm();
+						setSelectedRoomStyle(roomStyle);
+					"
+					@delete="deleteData(roomStyle._id)"
+					@moveRenders="handleMoveRenders(roomStyle._id)"
+					@publishOptions="handlePublishOptions(roomStyle._id)"
+				>
+				</card>
+			</div>
+		</div>
+		<v-btn
+			class="add-thumbnail"
+			fab
+			dark
+			color="indigo"
+			@click="
+				removeselectedTemplate();
+				openDialogForm();
+			"
+		>
+			<v-icon dark> mdi-plus </v-icon>
+		</v-btn>
+		<dialog-form
+			:roomStyle="selectedRoomStyle"
+			@refresh="getData()"
+		></dialog-form>
+		<v-overlay :value="showOverlayLoader">
+			<v-progress-circular
+				indeterminate
+				color="#5243AA"
+				size="64"
+			></v-progress-circular>
+		</v-overlay>
+	</div>
 </template>
 
 <script>
-import Card from '../components/Card.vue';
-import DialogForm from '../components/DialogForm.vue';
-import { mapGetters, mapMutations, mapActions } from 'vuex';
+import Card from "../components/Card.vue";
+import DialogForm from "../components/DialogForm.vue";
+import { mapGetters, mapMutations, mapActions } from "vuex";
 export default {
-    components: {
-        Card, DialogForm
-    },
-    data() {
-        return {
-            roomStyles: null,
-            selectedRoomStyle: null,
-            roomsGroupedByRoomStyles: [],
-        }
-    },
-    beforeMount() {
-        this.getData();
-    },
-    computed: {
-        ...mapGetters(["showDialogForm", "showOverlayLoader"]),
-    },
-    methods: {
-        ...mapMutations(["openDialogForm", "openOverlayLoader"]),
-        ...mapActions("crud", ["getRoomStyles", "deleteRoomStyle", "moveRenders", "publishOptions"]),
-        setSelectedRoomStyle(roomStyle) {
-            this.selectedRoomStyle = roomStyle;
-        },
-        removeselectedTemplate() {
-            this.selectedRoomStyle = null;
-        },
-        getData() {
-            console.log("hii")
-            this.getRoomStyles().then((data) => {
-                this.roomStyles = data
-                this.groupingByRoomType()
-            })
-        },
-        deleteData(id) {
-            if (window.confirm("Are you sure you want to delete this roomStyle")) {
-                this.openOverlayLoader();
-                this.deleteRoomStyle({
-                    _id: id
-                }).then((response) => {
-                    console.log(response)
-                    this.getData();
-                });
-            }
-        },
-        handleMoveRenders(id) {
-            if (window.confirm("Are you sure you want to Move the renders")) {
-                this.openOverlayLoader();
-                this.moveRenders({
-                    _id: id
-                }).then((response) => {
-                    console.log(response)
-                });
-            }
-        },
-        handlePublishOptions(id) {
-            if (window.confirm("Will publish all un publish Options , Are you sure you want to perform this action?")) {
-                this.openOverlayLoader();
-                this.publishOptions({
-                    _id: id
-                }).then((response) => {
-                    console.log(response)
-                });
-            }
-        },
-        groupingByRoomType() {
-            if (this.roomsGroupedByRoomStyles) this.roomsGroupedByRoomStyles = [];
-            this.roomStyles.forEach((room) => {
-                if (this.roomsGroupedByRoomStyles.includes(room.roomType)) {
-                    this.roomsGroupedByRoomStyles[room.roomType].push(room)
-                } else {
-                    this.roomsGroupedByRoomStyles.push(room.roomType)
-                    this.roomsGroupedByRoomStyles[room.roomType] = [{ ...room }]
-                }
-            })
-            console.log(this.roomsGroupedByRoomStyles)
-        },
-    }
-}
+	components: {
+		Card,
+		DialogForm,
+	},
+	data() {
+		return {
+			roomStyles: null,
+			selectedRoomStyle: null,
+			roomsGroupedByRoomStyles: [],
+		};
+	},
+	beforeMount() {
+		this.getData();
+		this.fetchAllDynamicProperties();
+	},
+	computed: {
+		...mapGetters(["showDialogForm", "showOverlayLoader"]),
+		...mapGetters("crud", ["dynamicProperties"]),
+	},
+	methods: {
+		...mapMutations(["openDialogForm", "openOverlayLoader"]),
+		...mapActions("crud", [
+			"getRoomStyles",
+			"deleteRoomStyle",
+			"moveRenders",
+			"publishOptions",
+			"fetchAllDynamicProperties",
+		]),
+		setSelectedRoomStyle(roomStyle) {
+			this.selectedRoomStyle = roomStyle;
+		},
+		removeselectedTemplate() {
+			this.selectedRoomStyle = null;
+		},
+		getData() {
+			console.log("hii");
+			this.getRoomStyles().then((data) => {
+				this.roomStyles = data;
+				this.groupingByRoomType();
+			});
+		},
+		deleteData(id) {
+			if (
+				window.confirm("Are you sure you want to delete this roomStyle")
+			) {
+				this.openOverlayLoader();
+				this.deleteRoomStyle({
+					_id: id,
+				}).then((response) => {
+					console.log(response);
+					this.getData();
+				});
+			}
+		},
+		handleMoveRenders(id) {
+			if (window.confirm("Are you sure you want to Move the renders")) {
+				this.openOverlayLoader();
+				this.moveRenders({
+					_id: id,
+				}).then((response) => {
+					console.log(response);
+				});
+			}
+		},
+		handlePublishOptions(id) {
+			if (
+				window.confirm(
+					"Will publish all un publish Options , Are you sure you want to perform this action?"
+				)
+			) {
+				this.openOverlayLoader();
+				this.publishOptions({
+					_id: id,
+				}).then((response) => {
+					console.log(response);
+				});
+			}
+		},
+		groupingByRoomType() {
+			if (this.roomsGroupedByRoomStyles)
+				this.roomsGroupedByRoomStyles = [];
+			this.roomStyles.forEach((room) => {
+				if (this.roomsGroupedByRoomStyles.includes(room.roomType)) {
+					this.roomsGroupedByRoomStyles[room.roomType].push(room);
+				} else {
+					this.roomsGroupedByRoomStyles.push(room.roomType);
+					this.roomsGroupedByRoomStyles[room.roomType] = [
+						{ ...room },
+					];
+				}
+			});
+			console.log(this.roomsGroupedByRoomStyles);
+		},
+	},
+};
 </script>
-
 
 <style lang="scss" scoped>
 .grouping-container {
-    margin: 2rem auto 0 auto;
-    max-width: 90vw;
+	margin: 2rem auto 0 auto;
+	max-width: 90vw;
 
-    &-category {
-        font-size: 1.6rem;
-        font-weight: 500;
-    }
+	&-category {
+		font-size: 1.6rem;
+		font-weight: 500;
+	}
 }
 
 .cards-container {
-    margin: 1rem auto;
-    max-width: 90vw;
-    display: grid;
-    grid-column-gap: 1rem;
-    grid-row-gap: 2%;
-    grid-template-columns: repeat(2, 48%);
+	margin: 1rem auto;
+	max-width: 90vw;
+	display: grid;
+	grid-column-gap: 1rem;
+	grid-row-gap: 2%;
+	grid-template-columns: repeat(2, 48%);
 }
 
 .add-thumbnail {
-    position: fixed;
-    right: 5%;
-    bottom: 3%;
+	position: fixed;
+	right: 5%;
+	bottom: 3%;
 }
 
 @media (max-width: 500px) {
-    .cards-container {
-        grid-row-gap: 2%;
-        grid-template-columns: 98%;
-    }
+	.cards-container {
+		grid-row-gap: 2%;
+		grid-template-columns: 98%;
+	}
 }
 
 @media (min-width: 750px) {
-    .cards-container {
-        grid-column-gap: 2%;
-        grid-row-gap: 2%;
-        grid-template-columns: repeat(2, 48%);
-    }
+	.cards-container {
+		grid-column-gap: 2%;
+		grid-row-gap: 2%;
+		grid-template-columns: repeat(2, 48%);
+	}
 }
 
 @media (min-width: 1000px) {
-    .cards-container {
-        grid-column-gap: 2%;
-        grid-row-gap: 2%;
-        grid-template-columns: repeat(3, 31.33%);
-    }
+	.cards-container {
+		grid-column-gap: 2%;
+		grid-row-gap: 2%;
+		grid-template-columns: repeat(3, 31.33%);
+	}
 }
 
 @media (min-width: 1440px) {
-    .cards-container {
-        grid-column-gap: 2%;
-        grid-row-gap: 2%;
-        grid-template-columns: repeat(4, 23%);
-    }
+	.cards-container {
+		grid-column-gap: 2%;
+		grid-row-gap: 2%;
+		grid-template-columns: repeat(4, 23%);
+	}
 }
 </style>
